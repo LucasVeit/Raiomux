f50c5be44c50871d66b64a61e5ea43654b4f7004
Lucas <Lucas-cs1998@hotmail.com>
2021-07-08 15:48:31 -0300

    Caesar in C++

--- ceasarCipher/caesarCipher.cpp
+++ ceasarCipher/caesarCipher.cpp
@@ -3,20 +3,35 @@
 
 using namespace std;
 
+
+int rot_n(int letter, int key, int value){
+    int rotted;
+    rotted = ((letter - value + key) % 26 ) + value;
+    return rotted;
+}
+
+int caesar(int letter, int key){
+    if(letter > 64 and letter < 91)
+        return rot_n(letter, key, 65);
+    else if(letter > 96 and letter < 123)
+        return rot_n(letter, key, 97);
+    return letter;
+}
+
 int main(){
     string original;
     string cipher;
     int key;
     cout << "Insert the original text:" << endl;
-    cin >> original;
+    getline(cin, original);
     cout << "Insert the Key Value" << endl;
     cin >> key;
 
     for(int i = 0; i < original.size(); ++i){
-        cipher += original[i] + key;
-        if(cipher[i] > 'z')
-            cipher[i] = cipher[i] - 26;
+        cipher += caesar(original[i], key);
     }
     
     cout << cipher << endl;
+
+    return 0;
 }
